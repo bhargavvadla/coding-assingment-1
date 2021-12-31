cb05f0840cecb51ea2aff09143b32d3c22514445
root <bhargavvadla4b6@gmail.com>
2021-12-31 16:04:00 +0000

    coding assingment 1 first commit

--- app.js
+++ app.js
@@ -56,78 +56,197 @@ app.get("/todos/", async (req, res) => {
   } = req.query;
 
   let todo,
-    warning = "None";
-  if (
-    status !== "TO DO" &&
-    status !== "IN PROGRESS" &&
-    status !== "DONE" &&
-    status !== ""
-  ) {
-    warning = "Invalid Todo Status";
-  } else if (
-    priority !== "HIGH" &&
-    priority !== "LOW" &&
-    priority !== "MEDIUM" &&
-    priority !== ""
-  ) {
-    warning = "Invalid Todo Priority";
-  } else if (
-    category !== "WORK" &&
-    category !== "HOME" &&
-    category !== "LEARNING" &&
-    category !== ""
-  ) {
-    warning = "Invalid Todo Category";
-  } else if (dueDate !== "") {
-    warning = "Invalid Due Date";
-  }
+    warningMsg = "None",
+    statusList = ["TO DO", "IN PROGRESS", "DONE", undefined, ""],
+    priorityList = ["HIGH", "MEDIUM", "LOW", undefined, ""],
+    searchList = ["TO DO", "IN PROGRESS", "DONE", undefined, ""],
+    categoryList = ["WORK", "HOME", "LEARNING", undefined, ""];
 
-  if (warning !== "None") {
+  if (
+    status === "" &&
+    priority === "" &&
+    search_q === "" &&
+    search_q === "" &&
+    category === "" &&
+    dueDate === ""
+  ) {
+    const todos = await db.all(`SELECT * FROM todo`);
+    res.send(
+      todos.map((eachTodo) => convertTodoDbObjectToResponseObject(eachTodo))
+    );
+  } else if (search_q !== "") {
+    const searchResult = await db.all(
+      `SELECT * FROM todo WHERE todo LIKE '%${search_q}%'`
+    );
+    res.status(200);
+    res.send(
+      searchResult.map((eachRes) =>
+        convertTodoDbObjectToResponseObject(eachRes)
+      )
+    );
+  } else if (!statusList.includes(status)) {
+    console.log(status);
+    warningMsg = "Invalid Todo Status";
     res.status(400);
-    res.send(warning);
+    res.send(warningMsg);
+  } else if (!priorityList.includes(priority)) {
+    warningMsg = "Invalid Todo Priority";
+    res.status(400);
+    res.send(warningMsg);
+  } else if (!priorityList.includes(priority)) {
+    warningMsg = "Invalid Todo Priority";
+    res.status(400);
+    res.send(warningMsg);
+  } else if (!categoryList.includes(category)) {
+    warningMsg = "Invalid Todo Category";
+    res.status(400);
+    res.send(warningMsg);
   } else {
-    if (category !== "" && status !== "") {
-      todo = await db.all(
-        `SELECT * FROM todo WHERE category='${category}' and status ='${status}'`
+    console.log("in else");
+    if (
+      categoryList.includes(category) &&
+      statusList.includes(status) &&
+      status !== "" &&
+      category !== ""
+    ) {
+      console.log("in cat sta");
+      const qRes = await db.all(
+        `SELECT * FROM todo WHERE category = '${category}'  and status = '${status}'`
       );
-      res.send(
-        todo.map((eachTodo) => convertTodoDbObjectToResponseObject(eachTodo))
+      res.status(200);
+      res.send(qRes);
+    } else if (
+      categoryList.includes(category) &&
+      priorityList.includes(priority) &&
+      priority !== "" &&
+      category !== ""
+    ) {
+      console.log("in cat pri");
+      const qRes = await db.all(
+        `SELECT * FROM todo WHERE category LIKE '${category}'  and priority LIKE '${priority}'`
       );
-    } else if (status !== "" && priority !== "") {
-      todo = await db.all(
-        `SELECT * FROM todo WHERE status='${status}' and priority='${priority}'`
+      res.status(200);
+      res.send(qRes);
+    } else if (categoryList.includes(category) && category !== "") {
+      console.log("in cat");
+      const qRes = await db.all(
+        `SELECT * FROM todo WHERE category ='${category}'`
       );
-      res.send(
-        todo.map((eachTodo) => convertTodoDbObjectToResponseObject(eachTodo))
+
+      res.status(200);
+      res.send(qRes);
+    } else if (
+      statusList.includes(status) &&
+      status !== "" &&
+      priority.includes(priority) &&
+      priority !== ""
+    ) {
+      console.log("in pri &  status");
+      const qRes = await db.all(
+        `SELECT * FROM todo WHERE status ='${status}' AND priority ='${priority}'`
       );
-    } else if (status !== "" && priority === "") {
-      todo = await db.all(`SELECT * FROM todo WHERE status='${status}'`);
-      res.send(
-        todo.map((eachTodo) => convertTodoDbObjectToResponseObject(eachTodo))
+      res.status(200);
+      res.send(qRes);
+    } else if (statusList.includes(status) && status !== "") {
+      console.log("in ony status");
+      const qRes = await db.all(`SELECT * FROM todo WHERE status ='${status}'`);
+      res.status(200);
+      res.send(qRes);
+    } else if (priorityList.includes(priority)) {
+      console.log("in ony priority");
+      const qRes = await db.all(
+        `SELECT * FROM todo WHERE priority ='${priority}'`
       );
-    } else if (category !== "" && priority === "") {
-      todo = await db.all(`SELECT * FROM todo WHERE category='${category}'`);
-      res.send(
-        todo.map((eachTodo) => convertTodoDbObjectToResponseObject(eachTodo))
-      );
-    } else if (status === "" && priority !== "") {
-      todo = await db.all(`SELECT * FROM todo WHERE priority='${priority}'`);
-      res.send(
-        todo.map((eachTodo) => convertTodoDbObjectToResponseObject(eachTodo))
-      );
-    } else if (search_q !== "") {
-      todo = await db.all(`SELECT * FROM todo WHERE todo LIKE '%${search_q}%'`);
-      res.send(
-        todo.map((eachTodo) => convertTodoDbObjectToResponseObject(eachTodo))
-      );
-    } else if (category !== "") {
-      todo = await db.all(`SELECT * FROM todo WHERE category ='${category}'`);
-      res.send(
-        todo.map((eachTodo) => convertTodoDbObjectToResponseObject(eachTodo))
-      );
+      res.status(200);
+      res.send(qRes);
     }
   }
 });
+
+// app.get("/todos/", async (req, res) => {
+//   const {
+//     status = "",
+//     priority = "",
+//     search_q = "",
+//     category = "",
+//     dueDate = "",
+//   } = req.query;
+
+//   let todo,
+//     warning = "None";
+
+//   if (
+//     status !== "TO DO" &&
+//     // status !== "IN PROGRESS" &&
+//     // status !== "DONE" &&
+//     status !== ""
+//   ) {
+//     warning = "Invalid Todo Status";
+//     console.log(status);
+//   } else if (
+//     priority !== "HIGH" &&
+//     priority !== "LOW" &&
+//     priority !== "MEDIUM" &&
+//     priority !== ""
+//   ) {
+//     warning = "Invalid Todo Priority";
+//   } else if (
+//     category !== "WORK" &&
+//     category !== "HOME" &&
+//     category !== "LEARNING" &&
+//     category !== ""
+//   ) {
+//     warning = "Invalid Todo Category";
+//   } else if (dueDate !== "") {
+//     warning = "Invalid Due Date";
+//   }
+
+//   if (warning !== "None") {
+//     res.status(400);
+//     res.send(warning);
+//   } else {
+//     if (category !== "" && status !== "") {
+//       todo = await db.all(
+//         `SELECT * FROM todo WHERE category='${category}' and status ='${status}'`
+//       );
+//       res.send(
+//         todo.map((eachTodo) => convertTodoDbObjectToResponseObject(eachTodo))
+//       );
+//     } else if (status !== "" && priority !== "") {
+//       todo = await db.all(
+//         `SELECT * FROM todo WHERE status='${status}' and priority='${priority}'`
+//       );
+//       res.send(
+//         todo.map((eachTodo) => convertTodoDbObjectToResponseObject(eachTodo))
+//       );
+//     } else if (status !== "" && priority === "") {
+//       todo = await db.all(`SELECT * FROM todo WHERE status='${status}'`);
+//       res.send(
+//         todo.map((eachTodo) => convertTodoDbObjectToResponseObject(eachTodo))
+//       );
+//     } else if (category !== "" && priority === "") {
+//       todo = await db.all(`SELECT * FROM todo WHERE category='${category}'`);
+//       res.send(
+//         todo.map((eachTodo) => convertTodoDbObjectToResponseObject(eachTodo))
+//       );
+//     } else if (status === "" && priority !== "") {
+//       todo = await db.all(`SELECT * FROM todo WHERE priority='${priority}'`);
+//       res.send(
+//         todo.map((eachTodo) => convertTodoDbObjectToResponseObject(eachTodo))
+//       );
+//     } else if (search_q !== "") {
+//       todo = await db.all(`SELECT * FROM todo WHERE todo LIKE '%${search_q}%'`);
+//       res.send(
+//         todo.map((eachTodo) => convertTodoDbObjectToResponseObject(eachTodo))
+//       );
+//     } else if (category !== "") {
+//       todo = await db.all(`SELECT * FROM todo WHERE category ='${category}'`);
+//       res.send(
+//         todo.map((eachTodo) => convertTodoDbObjectToResponseObject(eachTodo))
+//       );
+//     }
+//   }
+// });
 
 app.get("/todos/:todoId/", async (req, res) => {
   const { todoId } = req.params;
@@ -138,8 +257,13 @@ app.get("/todos/:todoId/", async (req, res) => {
 
 app.get("/agenda/", async (req, res) => {
   const { date } = req.query;
-  const todo = await db.get(`SELECT * FROM todo WHERE due_date='${date}'`);
-  res.send(convertTodoDbObjectToResponseObject(todo));
+  if (date !== "" && date !== undefined) {
+    const todo = await db.get(`SELECT * FROM todo WHERE due_date='${date}'`);
+    res.send(convertTodoDbObjectToResponseObject(todo));
+  } else {
+    res.status(400);
+    res.send("Invalid Due Date");
+  }
 });
 
 app.delete("/todos/:todoId/", async (req, res) => {
@@ -185,7 +309,7 @@ app.put("/todos/:todoId/", async (req, res) => {
     res.send("Category Updated");
   }
 
-  if (dueDate !== "") {
+  if (dueDate !== "" && dueDate !== undefined) {
     await db.run(`UPDATE todo SET due_date='${dueDate}'
       WHERE
       id=${todoId}`);
